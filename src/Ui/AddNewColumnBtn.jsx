9e87a0d633a1f93feb432e18a629a2a89125e552
brsekani <lawaltemidayo06@gmail.com>
2024-02-16 03:42:23 -0800

    Addressing the resizing issue of the new column button on certain viewports.

--- src/Ui/AddNewColumnBtn.jsx
+++ src/Ui/AddNewColumnBtn.jsx
@@ -1,6 +1,6 @@
 function AddNewColumnBtn() {
   return (
-    <span className="flex h-full items-center justify-center rounded-md font-bold text-[#828fa3]">
+    <span className="flex h-full w-[280px] items-center justify-center rounded-md text-lg font-bold text-[#828fa3] sm:text-xl md:text-2xl   ">
       + New Column
     </span>
   );
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -31,9 +31,9 @@ function Board() {
         </button>
       )}
 
-      <div className="h-fit">
+      <div className="h-fit ">
         {/* BUG */}
-        <div className=" flex h-full gap-8  px-8 py-6">
+        <div className="flex h-full gap-8 px-8 py-6">
           {/* Columns */}
           {boards.length > 0 ? (
             boards[currentBoardIndex]?.columns?.map((column, i) => (
@@ -53,7 +53,7 @@ function Board() {
       {boards.length === 0 ||
         boards?.[currentBoardIndex]?.columns.length === 5 || (
           <button
-            className="bg-linear-gradient-to-b mt-12 flex h-full w-[280px] items-center justify-center rounded-md font-bold"
+            className="bg-linear-gradient-to-b mr-10 mt-[4.5rem] flex h-full w-[280px] items-center justify-center rounded-md font-bold"
             onClick={() => dispatch(openAddNewColumn())}
           >
             <AddNewColumnBtn />
